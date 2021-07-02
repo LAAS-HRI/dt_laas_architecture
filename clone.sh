1e3b32d6bc6cce19ed5cf3f1b6bc07966213992e
Guillaume Sarthou <gsarthou@laas.fr>
2021-07-02 16:47:58 +0200

    [ADD] clone and update script

---
+++ clone.sh
@@ -0,0 +1,15 @@
+git clone https://github.com/LAAS-HRI/pr2_resources_management.git
+git clone https://github.com/LAAS-HRI/exp_director_task.git
+git clone https://github.com/LAAS-HRI/knowledge_sharing_planner_msgs.git
+git clone https://gitlab.com/laas-hri/knowledge_sharing_planner.git
+git clone https://github.com/LAAS-HRI/resource_management.git
+git clone https://github.com/sarthou/ontologenius.git
+git clone https://github.com/sarthou/mementar.git
+git clone https://github.com/sarthou/ar_track_alvar.git
+git clone https://github.com/sarthou/uwds3.git -b dev
+git clone https://github.com/sarthou/uwds3_msgs.git -b dev
+git clone https://github.com/amdia/ld_rjs.git
+git clone https://github.com/amdia/rjs.git
+git clone https://github.com/guilhembn/HATPEHDA.git
+git clone https://github.com/YannickRiou/pr2_mtc.git
+git clone https://github.com/YannickRiou/moveit.git -b pr2_mtc
---
+++ update.sh
@@ -0,0 +1,15 @@
+cd pr2_resources_management && git pull && cd ..
+cd exp_director_task && git pull && cd ..
+cd knowledge_sharing_planner_msgs && git pull && cd ..
+cd knowledge_sharing_planner && git pull && cd ..
+cd resource_management && git pull && cd ..
+cd ontologenius && git pull && cd ..
+cd mementar && git pull && cd ..
+cd ar_track_alvar && git pull && cd ..
+cd uwds3 && git pull && cd ..
+cd uwds3_msgs && git pull && cd ..
+cd ld_rjs && git pull && cd ..
+cd rjs && git pull && cd ..
+cd HATPEHDA && git pull && cd ..
+cd pr2_mtc && git pull && cd ..
+cd moveit && git pull && cd ..
